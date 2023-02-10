5db37af1ac24b7b5d2c3ab5954c933b15bf895d0
AlexBrand12345 <market201483@gmail.com>
2023-02-10 22:16:47 +0300

    Initial commit

---
+++ Lab1/helloworld.py
@@ -0,0 +1,40 @@
+print("Hello, world")
+
+groupmates = [
+    {
+        "name": "Александр",
+        "surname": "Иванов",
+        "exams": ["Информатика", "ЭЭиС", "Web"],
+        "marks": [4, 3, 5]
+    },
+    {
+        "name": "Иван",
+        "surname": "Петров",
+        "exams": ["История", "АиГ", "КТП"],
+        "marks": [4, 4, 5]
+    },
+    {
+        "name": "Кирилл",
+        "surname": "Смирнов",
+        "exams": ["Философия", "ИС", "КТП"],
+        "marks": [5, 5, 5]
+    },
+    {
+        "name": "Михаил",
+        "surname": "Данин",
+        "exams": ["РЯиКР", "Высшая математика", "ВВИТ"],
+        "marks": [3, 3, 5]
+    }
+]
+
+def print_students(students, marks_limit):
+    print(u"Имя".ljust(15), u"Фамилия".ljust(10), u"Экзамены".ljust(30), u"Оценки".ljust(20))
+    for student in students:
+        if sum(student["marks"])/len(student["marks"]) < marks_limit:
+            continue
+        print(student["name"].ljust(15), 
+              student["surname"].ljust(10), 
+              str(student["exams"]).ljust(30), 
+              str(student["marks"]).ljust(20))
+        
+print_students(groupmates, 4.1)
